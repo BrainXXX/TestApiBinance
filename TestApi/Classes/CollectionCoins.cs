4f31baf8d5a18d9e10f63b27f08dcd3a3dc4a2a7
BrainXXX <xxxbrainxxx@gmail.com>
2021-07-16 13:55:16 +1000

    Finished 16.07.2021

---
+++ TestApi/Classes/CollectionCoins.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace TestApi.Classes
+{
+    public class CollectionCoins : IComparable<CollectionCoins>
+    {
+        public string symbol { set; get; } //0
+        public double priceChange { get; set; } //1
+        public double priceChangePercent { get; set; } //2
+        public double weightedAvgPrice { get; set; } //3
+        public double lastPrice { get; set; } //4
+        public double lastQty { get; set; } //5
+        public double openPrice { get; set; } //6
+        public double highPrice { get; set; } //7
+        public double lowPrice { get; set; } //8
+        public double volume { get; set; } //9
+        public double quoteVolume { get; set; } //10
+        public ulong openTime { get; set; }
+        public ulong closeTime { get; set; }
+        public ulong firstId { get; set; }
+        public ulong lastId { get; set; }
+        public ulong count { get; set; }
+
+
+        public CollectionCoins() { }
+
+        public CollectionCoins(string symbol, double lastPrice, double quoteVolume)
+        {
+            this.symbol = symbol;
+            this.lastPrice = lastPrice;
+            this.quoteVolume = quoteVolume;
+        }
+
+        public CollectionCoins(string symbol, double priceChange, double priceChangePercent, double weightedAvgPrice, double lastPrice, double lastQty, double openPrice,
+            double highPrice, double lowPrice, double volume, double quoteVolume, ulong openTime, ulong closeTime, ulong firstId, ulong lastId, ulong count)
+        {
+            this.symbol = symbol;
+            this.priceChange = priceChange;
+            this.priceChangePercent = priceChangePercent;
+            this.weightedAvgPrice = weightedAvgPrice;
+            this.lastPrice = lastPrice;
+            this.lastQty = lastQty;
+            this.openPrice = openPrice;
+            this.highPrice = highPrice;
+            this.lowPrice = lowPrice;
+            this.volume = volume;
+            this.quoteVolume = quoteVolume;
+            this.openTime = openTime;
+            this.closeTime = closeTime;
+            this.firstId = firstId;
+            this.lastId = lastId;
+            this.count = count;
+        }
+
+        // Реализуем интерфейс IComparable<T>
+        public int CompareTo(CollectionCoins obj)
+        {
+            if (this.quoteVolume < obj.quoteVolume)
+                return 1;
+            if (this.quoteVolume > obj.quoteVolume)
+                return -1;
+            else
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-CA");
+            return $"{symbol,-20}{lastPrice,15:C2}{quoteVolume,20:C0}";
+        }
+    }
+}
---
+++ TestApi/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using TestApi.Classes;
+
+namespace TestApi
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string url1 = "https://fapi.binance.com/fapi/v1/ticker/price";
+        private readonly string url2 = "https://fapi.binance.com/fapi/v1/ticker/24hr";
+        private readonly string url3 = "https://fapi.binance.com/fapi/v1/ticker/24hr?symbol=BTCUSDT";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = await Task.Run(() => OutputWebResponse(url2));
+        }
+
+        private static async Task<string> getWebResponse(string url)
+        {
+            // create request..
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            // use GET method
+            webRequest.Method = "GET";
+
+            // POST!
+            HttpWebResponse webResponse = await webRequest.GetResponseAsync() as HttpWebResponse;
+
+            // read response into StreamReader
+            Stream responseStream = webResponse.GetResponseStream();
+            StreamReader _responseStream = new StreamReader(responseStream);
+
+            // get raw result
+            return _responseStream.ReadToEnd();
+        }
+
+        private static async Task<string> OutputWebResponse(string url)
+        {
+            try
+            {
+                string byc = await getWebResponse(url);
+
+                int pos1 = byc.IndexOf("[", 0);
+                int pos2 = byc.IndexOf("]", pos1);
+
+                string[] str1 = byc.Substring(pos1 + 1, pos2 - 1).Replace("{", "").Replace("\"", "").Split("},");
+
+                List<CollectionCoins> collectionCoins = new List<CollectionCoins>();
+                string[] s;
+
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    s = str1[i].Split(",");
+                    collectionCoins.Add(new CollectionCoins(s[0].Split(':')[1], Convert.ToDouble(s[4].Split(':')[1].Replace(".", ",")), 
+                        Convert.ToDouble(s[10].Split(':')[1].Replace(".", ","))));
+                }
+
+                collectionCoins.Sort();
+
+                string str2 = "";
+
+                foreach (CollectionCoins a in collectionCoins)
+                {
+                    str2 += a.ToString() + "\n";
+                }
+
+                return str2;
+            }
+            catch (Exception ex)
+            {
+                return ex.Source + " - " + ex.Message;
+            }
+        }
+    }
+}
